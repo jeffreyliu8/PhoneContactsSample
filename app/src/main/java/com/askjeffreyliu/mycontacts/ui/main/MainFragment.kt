55a2e2e2bd10a683da7d7bf5db176173d05ff133
Jeffrey Liu <jeffreyliu8@gmail.com>
2020-09-13 22:29:20 -0700

    add permission check

--- app/src/main/java/com/askjeffreyliu/mycontacts/ui/main/MainFragment.kt
+++ app/src/main/java/com/askjeffreyliu/mycontacts/ui/main/MainFragment.kt
@@ -1,11 +1,17 @@
 package com.askjeffreyliu.mycontacts.ui.main
 
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,6 +25,8 @@ class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
+        private const val PERMISSIONS_REQUEST_CODE = 123
+        private val PERMISSIONS = arrayOf(Manifest.permission.READ_CONTACTS)
     }
 
     private lateinit var viewModel: MainViewModel
@@ -26,6 +34,7 @@ class MainFragment : Fragment() {
 
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var mAdapter: MyAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +46,7 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initUIListeners()
         initList()
     }
 
@@ -46,20 +56,20 @@ class MainFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    private fun initUIListeners() {
+        permissionButton.setOnClickListener {
+            showSystemPermissionRequestWindow()
+        }
+    }
 
     private fun initList() {
-        val testList = mutableListOf<MyContact>()
-        testList.add(MyContact("asdf", "asdfsadf"))
-        testList.add(MyContact("asdfas", "asdfdfsadf"))
-
         viewManager = LinearLayoutManager(requireContext())
-        viewAdapter = MyAdapter {
+        mAdapter = MyAdapter {
             Logger.d(it.name)
-        }.apply {
-            updateList(testList)
         }
+        viewAdapter = mAdapter
 
-        recyclerView.apply {
+        contactRecyclerView.apply {
             // use this setting to improve performance if you know that changes
             // in content do not change the layout size of the RecyclerView
             setHasFixedSize(true)
@@ -69,7 +79,99 @@ class MainFragment : Fragment() {
 
             // specify an viewAdapter (see also next example)
             adapter = viewAdapter
-
         }
+    }
+
+    private fun populateListData() {
+        val testList = mutableListOf<MyContact>()
+        testList.add(MyContact("asdf", "asdfsadf"))
+        testList.add(MyContact("asdfas", "asdfdfsadf"))
+        mAdapter.updateList(testList)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        tryGetPermissionAndExecuteTask()
+    }
+
+    private fun tryGetPermissionAndExecuteTask() {
+        // check if we have the permission
+        if (!hasPermissions(requireContext(), PERMISSIONS[0])) {
+            // if not, we explain why we need it first
+            AlertDialog.Builder(requireContext())
+                .setTitle("Permission Request")
+                .setMessage("With these permissions, we can operate the peripherals")
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    showSystemPermissionRequestWindow()
+                }
+                .show()
+            permissionButton.visibility = View.VISIBLE
+        } else { // we already have permission
+            permissionButton.visibility = View.GONE
+            populateListData()
+        }
+    }
+
+    private fun hasPermissions(context: Context, vararg permissions: String): Boolean =
+        permissions.all {
+            ActivityCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+        }
+
+    private fun showSystemPermissionRequestWindow() {
+        // Should we show an explanation?
+//        if (ActivityCompat.shouldShowRequestPermissionRationale(
+//                requireActivity(),
+//                PERMISSIONS[0]
+//            )
+//        ) {
+//
+//            textView.setText("Got rejected the first time, and now we need this permission again")
+//            // Show an explanation to the user *asynchronously* -- don't block
+//            // this thread waiting for the user's response! After the user
+//            // sees the explanation, try again to request the permission.
+//        }
+
+        requestPermissions(
+            PERMISSIONS,
+            PERMISSIONS_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    allPermissionsCheckGranted(grantResults)
+                ) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    // add your logic here
+                    permissionButton.visibility = View.GONE
+                    populateListData()
+                } else {
+                    Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_LONG).show()
+                    permissionButton.visibility = View.VISIBLE
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+        }// other 'case' lines to check for other
+        // permissions this app might request
+    }
+
+    private fun allPermissionsCheckGranted(grantResults: IntArray): Boolean {
+        for (grantResult in grantResults) {
+            if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
     }
 }
